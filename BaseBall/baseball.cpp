97840a713f0e5e9a97c8f340840b83b53b7fc7d5
Kim, dong su <hucke@hanmail.net>
2023-06-02 10:08:41 +0900

    업데이트

--- BaseBall/baseball.cpp
+++ BaseBall/baseball.cpp
@@ -1,4 +1,72 @@
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+struct Result
+{
+    bool solved;
+    int strike;
+    int balls;
+};
+
 class Baseball
 {
 public:
+    Baseball(string question)
+    {
+        question_ = question;
+    }
+
+    bool isDuplicatedNumber(std::string guessNumber)
+    {
+        return guessNumber[0] == guessNumber[1] ||
+            guessNumber[0] == guessNumber[2] ||
+            guessNumber[1] == guessNumber[2];
+    }
+
+    bool isIncludeChar(std::string guessNumber)
+    {
+        for (auto ch : guessNumber)
+        {
+            if (ch >= '0' && ch <= '9')
+            {
+                return true;
+            }
+        }
+    }
+
+    void assertIllegalArguments(std::string guessNumber)
+    {
+        if (guessNumber == "")
+        {
+            throw std::invalid_argument("값을 넣어야함.");
+        }
+        if (guessNumber.length() != 3)
+        {
+            throw std::invalid_argument("자리수 오류.");
+        }
+        if (isIncludeChar(guessNumber))
+        {
+            throw std::invalid_argument("자리수 오류.");
+        }
+        if (isDuplicatedNumber(guessNumber))
+        {
+            throw std::invalid_argument("중복수 금지.");
+        }
+    }
+
+    Result guess(std::string guessNumber)
+    {
+        assertIllegalArguments(guessNumber);
+        if (guessNumber == question_)
+        {
+            return { true, 3, 0 };
+        }
+
+        return { false, 0, 0 };
+    }
+
+private:
+    string question_;
 };
--- Sample-Test1/baseball_test.cpp
+++ Sample-Test1/baseball_test.cpp
@@ -1,5 +1,19 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
+#include "../BaseBall/baseball.cpp"
+
+TEST(BaseballGame, InvalidArgument) {
+    Baseball baseball("123");
+    EXPECT_THROW(baseball.guess(""), std::invalid_argument);
+    EXPECT_THROW(baseball.guess("1543"), std::invalid_argument);
+    EXPECT_THROW(baseball.guess("1v3"), std::invalid_argument);
+    EXPECT_THROW(baseball.guess("121"), std::invalid_argument);
+}
+
+TEST(BaseballGame, PerfectMatch) {
+    Baseball baseball("123");
+    Result result = baseball.guess("123");
+    EXPECT_EQ(true, result.solved);
+    EXPECT_EQ(3, result.strike);
+    EXPECT_EQ(0, result.balls);
 }
